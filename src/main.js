b4d5d1a0873c780a16a68892b0216fb21e8af4fa
Yannik Ehlert <kontakt@yanniks.de>
2019-05-28 13:38:39 +0200

    Update Vue.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Buefy from 'buefy'
-import 'buefy/lib/buefy.css'
+import 'buefy/dist/buefy.css'
 
 Vue.use(Buefy)
 
@@ -14,6 +14,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: {App},
+  components: { App },
   template: '<App/>'
 })
